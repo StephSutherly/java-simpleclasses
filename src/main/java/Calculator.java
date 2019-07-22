b4795d7129585b6e17755c3e8839ad9416bc0e9b
Steph Scully <stephanielarascullion@yahoo.com>
2019-07-22 16:41:16 +0100

    Finished task 1 - calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,31 @@
 public class Calculator {
+
+    private double numOne;
+    private double numTwo;
+
+    public Calculator(double numOne, double numTwo) {
+        this.numOne = numOne;
+        this.numTwo = numTwo;
+    }
+
+    public double addNums() {
+       return numOne + numTwo;
+    }
+
+    public double subtractNums() {
+        return numOne - numTwo;
+    }
+
+    public double multiplyNums() {
+        return numOne * numTwo;
+    }
+
+    public double divideNums() {
+        return numOne / numTwo;
+    }
 }
+
+
+
+
+
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void before() {
+        calculator = new Calculator(20.22, 3.00);
+    }
+
+    @Test
+    public void canAdd() {
+        assertEquals(23.22, calculator.addNums(), 0.01);
+    }
+
+    @Test
+    public void canSubtract() {
+        assertEquals(17.22, calculator.subtractNums(), 0.01);
+    }
+
+    @Test
+    public void canMultiply() {
+        assertEquals(60.66, calculator.multiplyNums(), 0.01);
+    }
+
+    @Test
+    public void canDivide() {
+        assertEquals(6.74, calculator.divideNums(), 0.01);
+    }
 }
+
+
