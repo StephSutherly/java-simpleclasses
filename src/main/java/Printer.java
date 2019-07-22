9e085456137351ba0ae110b1d12f8be392e158ad
Steph Scully <stephanielarascullion@yahoo.com>
2019-07-22 17:14:18 +0100

    Started extension

---
+++ src/main/java/Printer.java
@@ -0,0 +1,18 @@
+public class Printer {
+
+    private int sheetsLeft;
+
+    public Printer(int sheetsLeft) {
+        this.sheetsLeft = sheetsLeft;
+    }
+
+    public int getSheetsNum() {
+        return this.sheetsLeft;
+    }
+
+    public void print(sheetsLeft, numOfCopies) {
+
+    }
+
+}
+
---
+++ src/test/java/PrinterTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PrinterTest {
+
+
+    private Printer printer;
+
+    @Before
+    public void before() {
+        printer = new Printer(100);
+    }
+
+    @Test
+    public void hasSheets() {
+        assertEquals(100, printer.getSheetsNum());
+    }
+}
